35d192b79515a76d7b2f1f2ab4000b6bbef5e76e
tu-z <dangvantu610@gmail.com>
2021-10-06 20:38:44 +0700

    upload

---
+++ src/Circumference/Circumference.java
@@ -0,0 +1,9 @@
+package Circumference;
+import java.util.*;
+public class Circumference {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double area = input.nextDouble();
+		System.out.println(2*3.14*area);
+	}
+}
