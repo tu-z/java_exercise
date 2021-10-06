df0629f4d78dd8384ae896af62c9ab5353338b76
tu-z <dangvantu610@gmail.com>
2021-10-06 20:45:30 +0700

    update

---
+++ src/First_degree_equation/First_degree_equation.java
@@ -0,0 +1,13 @@
+package First_degree_equation;
+import java.util.Scanner;
+
+public class First_degree_equation {
+	private static Scanner input = new Scanner(System.in);
+	 public static void main(String[] args) {
+		 System.out.println("nhap a");
+		 double a = input.nextDouble();
+		 System.out.println("nhap b");
+		 double b = input.nextDouble();
+		 System.out.println(a+"x" + " + "+b+" = 0 => x = " +(-b/a) );
+	 }
+}
